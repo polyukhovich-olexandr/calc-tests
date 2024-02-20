ed972b53b393d7a3c9473f343b15b35c58db8856
polyukhovich-olexandr <leanl3012@gmail.com>
2024-02-20 22:10:25 +0200

    added simple calculations

---
+++ calculations.py
@@ -0,0 +1,23 @@
+# calculations.py
+
+
+def add(a, b):
+    return float(a + b)
+
+
+def subtract(a, b):
+    return float(a - b)
+
+
+def multiply(a, b):
+    return float(a * b)
+
+
+def divide(a, b):
+    return float(a / b)
+
+
+print(f"2 + 3 = {add(2, 3)}")
+print(f"2 - 3 = {subtract(2, 3)}")
+print(f"2 * 3 = {multiply(2, 3)}")
+print(f"2 / 3 = {divide(2, 3)}")
